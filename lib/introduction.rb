0b4384fc7985bcaa105c4ecd72c5dd8338463c16
Mary Selig <seligmar@gmail.com>
2019-06-18 16:37:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,11 @@
-#Define a Method That Takes in and Uses an Argument
-#Define a method, #introduction that takes in the argument name and outputs the phrase: "Hi, my name is #{name}."
+ def introduction(name)
+  puts "Hi, my name is #{name}."
+end  
 
-def introduction(name)
-  puts ""
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
-#Define a Method That Takes in and Uses Two Arguments
-#Define a method, #introduction_with_language that takes in two arguments, name and language and outputs the phrase: "Hi, my name is #{name} and I am learning to program in #{language}."
-
-Define a Method With an Optional Argument
-Copy the previous method and name it #introduction_with_language_optional. It that takes in two arguments,name and language, but language is optional. The default value for language will be "Ruby".
+def introduction_with_language_optional(name, language = "Ruby")
+   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
